b5365c1f177b085f4379c6249785142e86e6b601
redetdereje <redetdereje91@gmail.com>
2022-07-01 18:04:54 -0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'screens/input_page.dart';
+import 'constants.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -6,29 +8,14 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor:
+            Color(0xFF0A0C22), //WONT WORK? still shows default color...
+        // only started working (on first page) bc I changed the app bar's color
+        // in input_page.dart
+        scaffoldBackgroundColor: Color(0xFF0A0C22), // works
+      ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
